7621f653f3cd16ef6ebf3105424b9d27f8451a69
patriciafelipe <patricia-felipe@hotmail.com>
2018-08-23 20:16:56 -0300

    aula javascript

---
+++ js/funcoes.js
@@ -0,0 +1,12 @@
+function trocaImagem(){
+	document.getElementById("myImage").src="img/html-logo-badge-512.png";
+}
+function imagemTroca(){
+	document.getElementById("myImage").src="img/js-logo-badge-512.png";
+}
+function trocaFonte(){
+	document.getElementById("demo").style.fontSize="35px"
+}
+function fonteTroca(){
+	document.getElementById("demo").style.fontSize="20px"
+}
