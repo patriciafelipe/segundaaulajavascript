9d8dda07e27bf7f58a0c02748aefd41d95a233ad
patriciafelipe <patricia-felipe@hotmail.com>
2018-08-23 21:44:14 -0300

    aula javascript

--- js/funcoes.js
+++ js/funcoes.js
@@ -5,8 +5,17 @@ function imagemTroca(){
 	document.getElementById("myImage").src="img/js-logo-badge-512.png";
 }
 function trocaFonte(){
-	document.getElementById("demo").style.fontSize="35px"
+	document.getElementById("demo").style.fontSize="35px";
 }
 function fonteTroca(){
-	document.getElementById("demo").style.fontSize="20px"
+	document.getElementById("demo").style.fontSize="20px";
+}
+function ocultarElemento(){
+	document.getElementById("demo").style.display="none";
+}
+function elementoOcultar(){
+	document.getElementById("demo").style.display="block";
+}
+function calcular(){
+	document.getElementById("demo").innerHTML = 10 + 5;;
 }
